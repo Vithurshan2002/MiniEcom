ade7a5aee0b183b4fa1089c1d562dabcaa11ee0e
Vithurshan2002 <sangervithu2002@gmail.com>
2025-09-11 00:56:21 +0530

    homepage created

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Homepage = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="flex flex-col sm:flex-row justify-between items-center sm:px-30 bg-gradient-to-tr from-blue-700 pt-20 sm:pt-0 to-blue-100">
+      <div>
+        <h1 className="sm:text-5xl text-3xl px-2 font-bold font-[poppins] text-center text-white"> Welcome to BitZa ,Where<br/> Every Bite is a Delight!</h1>
+        <div className="flex justify-center space-x-4 py-6">
+          <buttton className='block font-bold  p-2 bg-blue-800 cursor-pointer hover:bg-blue-400 transition-all px-2  rounded text-white'>Contact Us</buttton>
+          <buttton  className='block font-bold  p-2 bg-green-800 cursor-pointer hover:bg-green-400 transition-all   rounded text-white'>Explore</buttton>
+        </div>
+      </div>
 
-export default Homepage
+      <div>
+        <img src="home.png" alt="noImage"  />
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -10,56 +10,86 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="flex justify-between px-5 items-center py-4 bg-gradient-to-r from-gray-100 to-blue-300">
-        <h1 className="text-4xl font-extrabold  from-blue-900">
+      <div className="flex justify-between px-5 items-center py-4 bg-gradient-to-r from-gray-100 to-blue-300 sticky top-0">
+        <h1 className="text-5xl font-extrabold  from-blue-900">
           Bit<span className="text-blue-800">Za</span>
         </h1>
-        <ul className="sm:flex space-x-2 sm:space-x-10 hidden">
-          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105">
+        <ul className="sm:flex space-x-2 sm:space-x-5 md:space-x-10 md: hidden">
+          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105 text-xl">
             Home
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105 text-xl">
             Dishes
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105 text-xl">
             About
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105 text-xl">
             Menu
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105 text-xl">
             Reviews
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:underline underline-offset-3 hover:text-emerald-700 transition-all hover:scale-105 text-xl">
             Order
           </li>
         </ul>
 
         <div className="flex items-center space-x-2 ">
-          <div className="flex sm:hidden hover:bg-green-400 rounded-lg transition-all cursor-pointer" onClick={()=>{
-                         const scn = document.getElementsByClassName("scn");
-                         scn[0].classList.toggle('translate-x-200')
-                         
-          }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="42"
-              height="40"
-              viewBox="0 0 24 24"
-              className="text-blue-800 "
-            >
-              <path
-                fill="currentColor"
-                d="M4 17.27v-1h16v1zm0-4.77v-1h16v1zm0-4.77v-1h16v1z"
-              />
-            </svg>
-          </div>
-          <div>
+          <div
+            className="flex sm:hidden hover:bg-blue-200 rounded-lg transition-all cursor-pointer"
+            onClick={() => {
+              const scn = document.getElementsByClassName("scn");
+              scn[0].classList.toggle("translate-x-200");
+            }}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="32"
               height="32"
               viewBox="0 0 24 24"
+              className="text-white"
+            >
+              <g
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              >
+                <path d="M5 5L19 19M5 19L19 5">
+                  <animate
+                    fill="freeze"
+                    attributeName="d"
+                    dur="0.4s"
+                    values="M5 5L19 19M5 19L19 5;M5 5L19 5M5 19L19 19"
+                  />
+                </path>
+                <path d="M12 12H12" opacity="0">
+                  <animate
+                    fill="freeze"
+                    attributeName="d"
+                    begin="0.2s"
+                    dur="0.4s"
+                    values="M12 12H12;M5 12H19"
+                  />
+                  <set
+                    fill="freeze"
+                    attributeName="opacity"
+                    begin="0.2s"
+                    to="1"
+                  />
+                </path>
+              </g>
+            </svg>
+          </div>
+          <div className="cursor-pointer ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              className="text-green-600 hover:text-black transition-all"
             >
               <path
                 fill="currentColor"
@@ -70,24 +100,24 @@ const Navbar = () => {
         </div>
       </div>
       {/* for mobile  */}
-      <div className=" bg-gradient-to-br from-emerald-500 to-blue-200 bg-transparent absolute w-full sm:hidden  scn  duration-600 ease-in-out ">
+      <div className=" border-t-4 border-b  border-y-blue-800  bg-gradient-to-br from-emerald-200 from-20% to-blue-200 absolute w-full sm:hidden  scn  duration-600 ease-in-out ">
         <ul className="text-2xl flex flex-col text-center space-y-2 ">
-          <li className="font-bold font-[poppins] cursor-pointer hover:bg-green-300 hover:text-white   w-full  transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:bg-blue-500 hover:text-white   w-full  transition-all hover:scale-105">
             Home
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:bg-green-500 hover:text-white  w-full transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:bg-blue-500 hover:text-white  w-full transition-all hover:scale-105">
             Dishes
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:bg-green-500 hover:text-white  w-full  transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:bg-blue-500 hover:text-white  w-full  transition-all hover:scale-105">
             About
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:bg-green-500 hover:text-white  w-full  transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:bg-blue-500 hover:text-white  w-full  transition-all hover:scale-105">
             Menu
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:bg-green-500 hover:text-white  w-full  transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:bg-blue-500 hover:text-white  w-full  transition-all hover:scale-105">
             Reviews
           </li>
-          <li className="font-bold font-[poppins] cursor-pointer hover:bg-green-500 hover:text-white  w-full  transition-all hover:scale-105">
+          <li className="font-bold font-[poppins] cursor-pointer hover:bg-blue-500 hover:text-white  w-full  transition-all hover:scale-105">
             Order
           </li>
         </ul>
